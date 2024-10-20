566d639402c5b2f0e1b4156ea09c3ac5f7a03555
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-10-20 15:45:45 +0500

    Working..

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,47 @@
-import { Text, View , StyleSheet } from "react-native";
+import { Text, View , StyleSheet, SafeAreaView, TextInput } from "react-native";
 
 export default function Index() {
+  
   return (
+    <View style={styles.container} >
     <View>
       <Text style = {styles.h1}>Todo App</Text>
     </View>
+
+    <View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        // onChangeText={onChangeNumber}
+        // value={number}
+        placeholder="Enter todo"  
+      />
+    </SafeAreaView>      
+    </View>
+    </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
+container:{
+
+},
+
 h1:{
 marginTop:10,
 fontSize:30,
 padding:10,
 textAlign:"center",
-fontFamily:"Georgia"
+fontFamily:"Georgia",
+backgroundColor:"#000000",
+color:"white",
+},
+input: {
+  height: 40,
+  margin: 12,
+  borderWidth: 1,
+  padding: 10,
+},
 
-}
 })
